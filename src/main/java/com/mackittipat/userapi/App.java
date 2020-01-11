5f7d7ebbbd3076d534b40792957da7db494eb702
Mac <mac.kittipat@gmail.com>
2020-01-11 13:03:28 +0700

    Init project

---
+++ src/main/java/com/mackittipat/userapi/App.java
@@ -0,0 +1,13 @@
+package com.mackittipat.userapi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

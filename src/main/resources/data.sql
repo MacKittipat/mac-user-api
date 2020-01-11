f688e4c0489f8a599b4d53d5846aa93b21c7d674
Mac <mac.kittipat@gmail.com>
2020-01-11 18:25:54 +0700

    Add more user

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,31 @@
 INSERT INTO user (username, email, password) VALUES
     ('user', 'user@user.com', 'password'),
-    ('admin', 'admin@admin.com', 'password');
+    ('user2', 'user2@admin.com', 'password'),
+    ('user3', 'user3@admin.com', 'password'),
+    ('user4', 'user4@admin.com', 'password'),
+    ('user5', 'user5@admin.com', 'password'),
+    ('user6', 'user6@admin.com', 'password'),
+    ('admin', 'admin@admin.com', 'password'),
+    ('admin2', 'admin2@admin.com', 'password'),
+    ('admin3', 'admin3@admin.com', 'password'),
+    ('admin4', 'admin4@admin.com', 'password')
+;
 
 INSERT INTO role (name) VALUES
     ('ROLE_USER'),
-    ('ROLE_ADMIN');
+    ('ROLE_ADMIN')
+;
 
 INSERT INTO user_role(user_id, role_id) VALUES
     (1, 1),
-    (2, 2);
+    (2, 1),
+    (3, 1),
+    (4, 1),
+    (5, 1),
+    (6, 1),
+    (7, 2),
+    (8, 2),
+    (9, 2),
+    (10, 2)
+;
 

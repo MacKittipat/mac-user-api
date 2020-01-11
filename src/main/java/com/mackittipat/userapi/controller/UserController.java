4a037fcf3e80466bf56d645b2b5621939869729a
Mac <mac.kittipat@gmail.com>
2020-01-11 17:44:28 +0700

    Create search user by role API

--- src/main/java/com/mackittipat/userapi/controller/UserController.java
+++ src/main/java/com/mackittipat/userapi/controller/UserController.java
@@ -4,6 +4,7 @@ import com.mackittipat.userapi.dto.User;
 import com.mackittipat.userapi.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,9 +20,9 @@ public class UserController {
         return userRepository.findAll();
     }
 
-    @GetMapping("users/admins")
-    public Iterable<User> findAdmins() {
-        return userRepository.findAll();
+    @GetMapping("users/{roleName}")
+    public Iterable<User> findUsersByRole(@PathVariable String roleName) {
+        return userRepository.findByRoleName(roleName);
     }
 
 }
--- src/main/java/com/mackittipat/userapi/repository/UserRepository.java
+++ src/main/java/com/mackittipat/userapi/repository/UserRepository.java
@@ -1,11 +1,21 @@
 package com.mackittipat.userapi.repository;
 
 import com.mackittipat.userapi.dto.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 public interface UserRepository extends CrudRepository<User, Long> {
 
     User findByUsername(String username);
 
-
+    @Query(value = "SELECT " +
+            "u.* " +
+            "FROM user as u, role as r, user_role as ur " +
+            "WHERE " +
+            "u.id=ur.user_id AND " +
+            "r.id=ur.role_id AND " +
+            "r.name=?1", nativeQuery = true)
+    List<User> findByRoleName(String roleName);
 }
